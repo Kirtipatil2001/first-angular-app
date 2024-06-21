789fbcd3b5f7d7c82f7285ac6bae5e34be223f77
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-06-21 15:39:11 +0530

    Assets added
    Title Updated and new user asset added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()  *DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,5 +11,8 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+    selectedUser = DUMMY_USERS[randomIndex];
+
 
 }
