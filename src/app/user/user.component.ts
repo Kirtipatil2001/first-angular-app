d0c17d63efa53bc4d3e27da09c7bca4a9f32b334
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-06-26 17:48:04 +0530

    required and optional inputs
    How to use required and optional inputs in angular

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,8 +12,8 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required :true}) avatar!: string;
+  @Input({required :true}) name!: string;
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
