3b333a09580635de23773c82cca8e2796dd6553a
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-07-18 18:12:33 +0530

    two waydata binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,49 +1,25 @@
-import { Component, EventEmitter, Input, Output, computed, input, output, signal } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-
-//TYPE ALIASES CONCEPT
-type User = {
-  id: string;
- avatar:string;
-name:string;
-}
-//const randomIndex = Math.floor(Math.random()  *DUMMY_USERS.length)
-
-// INTERFACE CONCEPT 
-
-//interface User{
-  //id: string;
- // avatar:string;
-  //name:string;
-//}
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CardComponent]
 })
-
 export class UserComponent {
-  //@Input({required :true}) id!: string;
-  //@Input({required :true}) avatar!: string;
-  //@Input({required :true}) name!: string;
-  @Input({required: true}) user!:User;
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
-  @Output() select = new EventEmitter();
-  //select = output<string>();  //it is new use of output function 
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 
-  imagePath(){
-    return 'assets/users/' + this.user;
-
-  };
-
-   onSelectUser(){
-        this.select.emit(this.user.id);
-   }
-    }  
-
-    
-     
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
