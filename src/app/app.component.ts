cbf9cc7ccd4957dfd97fe5f78e97e54c79102517
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-07-19 11:59:34 +0530

    Complete task
    Conditional Rendering
    Using directives and Two-Way-Binding
    Signals
    Content projection
    Service usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
+
 
 @Component({
     selector: 'app-root',
@@ -15,13 +16,12 @@ import { TasksComponent } from "./tasks/tasks.component";
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
-  
-  get selectedUser(){
-    return this.users.find((user) => user.id == this.selectedUserId);
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
-  onSelectUser(id:string) {
-     this.selectedUserId = id;
-    
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
   }
 }
